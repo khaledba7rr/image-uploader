d77d421688a3f86f2105276a215292189e5796cb
Khaled Ahmed <khaled.ahmed0011@gmail.com>
2023-06-26 20:12:06 +0200

    footer with photo extension verification

--- src/app/Directives/drag-and-drop.directive.ts
+++ src/app/Directives/drag-and-drop.directive.ts
@@ -10,17 +10,34 @@ export class DragAndDropDirective {
   
   @HostBinding('class.fileover') fileOver : boolean = false;
 
+  @HostBinding('class.filewrong') invalidFile : boolean = false;
+
   @Output() fileDropped = new EventEmitter<FileHandler>();
 
-  constructor(private sanitizer: DomSanitizer) {        
-  }
+  constructor(private sanitizer: DomSanitizer) {}
+
+  allowedExtensions = /(\.jpg|\.jpeg|\.png)$/i;
+
+  imagesExts: any[] = [
+    'image/jpg',
+    'image/jpeg',
+    'image/png',
+  ]
 
   @HostListener('dragover', ['$event']) onDragOver(evt : DragEvent){
     evt.preventDefault();
     evt.stopPropagation();
 
+   if(this.imagesExts.indexOf(evt.dataTransfer?.items[0]?.type) !== -1){
+    console.log('found');
+    this.invalidFile = false;    
+   }else{
+    this.invalidFile = true;
+    console.log(this.invalidFile);
+    
+   }
+
     this.fileOver = true;
-    console.log('drag over');
   }
 
   @HostListener('dragleave', ['$event']) public onDragLeave(evt : DragEvent){
@@ -29,7 +46,6 @@ export class DragAndDropDirective {
 
     this.fileOver = false;
 
-    console.log('drag leave');
   }
 
   @HostListener('drop', ['$event'])public onDrop(evt: DragEvent){
@@ -37,18 +53,20 @@ export class DragAndDropDirective {
     evt.stopPropagation();
 
     this.fileOver = false;
-    console.log('dropped');
 
     let file = evt.dataTransfer?.files[0];
-
+    
     let fileHandler : FileHandler;
 
     if(file){
+      this.invalidFile = false;
+      if(this.allowedExtensions.exec(file.name)){
         fileHandler = {
           file : file,
           path : this.sanitizer.bypassSecurityTrustUrl(window.URL.createObjectURL(file))
         }
         this.fileDropped.emit(fileHandler);
+      }
     }
 
   }
